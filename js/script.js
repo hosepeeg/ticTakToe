b7c3d697af707ea8209bf25b99e77cf74449ca84
hosepeeg <hkourbouchian@gmail.com>
2022-07-18 17:29:55 -0400

    added event listers + popup

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-//will be using https://michalosman.github.io/tic-tac-toe/ as a reference
+//-------------------Creating all element selectors-------------------
+const playerButton = document.getElementById("player");
+const aiButton = document.getElementById("AI");
+const restart = document.getElementById("reset-button");
+
+
+
+
+//-------------------creating global variables-------------------
+let versus = "";
+
+
+
+
+//-------------------event handlers-------------------
+document.querySelector('#player').addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    versus = playerButton.innerHTML;
+});
+
+document.querySelector('#AI').addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    versus = aiButton.innerHTML;
+});
+
+document.querySelector('#reset-button').addEventListener("click", function(){
+    window.location.reload();
+});
